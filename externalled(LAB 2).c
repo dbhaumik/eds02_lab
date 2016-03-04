ab76dd566e43c66dbe093b4f7b95c49e63559e53
dbhaumik <dbhaumik576@gmail.com>
2016-03-04 13:11:28 +0530

    third

---
+++ externalled(LAB 2).c
@@ -0,0 +1,108 @@
+                                         //exp 2
+//PORTD DEFINITION
+
+#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
+#define GPIO_PORTD_DATA_R       (*((volatile unsigned long *)0x400073FC))
+#define GPIO_PORTD_DIR_R        (*((volatile unsigned long *)0x40007400))
+#define GPIO_PORTD_AFSEL_R      (*((volatile unsigned long *)0x40007420))
+#define GPIO_PORTD_DEN_R        (*((volatile unsigned long *)0x4000751C))
+#define GPIO_PORTD_AMSEL_R      (*((volatile unsigned long *)0x40007528))
+#define GPIO_PORTD_PCTL_R       (*((volatile unsigned long *)0x4000752C))
+	
+#define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
+#define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
+#define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
+#define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))
+#define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C))
+#define GPIO_PORTF_LOCK_R       (*((volatile unsigned long *)0x40025520))
+#define GPIO_PORTF_CR_R         (*((volatile unsigned long *)0x40025524))
+#define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528))
+#define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))
+#define PF4                     (*((volatile unsigned long *)0x40025040))
+#define PF3                     (*((volatile unsigned long *)0x40025020))
+#define PF2                     (*((volatile unsigned long *)0x40025010))
+#define PF1                     (*((volatile unsigned long *)0x40025008))
+#define PF0                     (*((volatile unsigned long *)0x40025004))
+#define GPIO_PORTF_DR2R_R       (*((volatile unsigned long *)0x40025500))
+#define GPIO_PORTF_DR4R_R       (*((volatile unsigned long *)0x40025504))
+#define GPIO_PORTF_DR8R_R       (*((volatile unsigned long *)0x40025508))
+#define GPIO_LOCK_KEY           0x4C4F434B  // Unlocks the GPIO_CR register
+#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
+	
+void PortD_Init(void);
+void PortF_Init(void);
+void Delay(unsigned long time);
+unsigned long in1,in2;
+
+int main(void){
+	long led;
+	PortD_Init();
+	PortF_Init();
+	
+	
+
+	
+	while(1)
+	{
+		in1=GPIO_PORTF_DATA_R & 0x10;
+		in2=GPIO_PORTF_DATA_R & 0x01;
+		if(!in1 && !in2)
+		{GPIO_PORTD_DATA_R = 0x09;
+		}else if(!in1 && in2)
+		{GPIO_PORTD_DATA_R = 0x08;
+		}else if(in1 && !in2)
+		{GPIO_PORTD_DATA_R = 0x01;
+		}
+		else if(in1 && in2)
+		{GPIO_PORTD_DATA_R = 0x00;
+		}
+	}
+		
+	}
+	
+
+
+
+void PortD_Init(void)
+	{volatile unsigned long delay;
+		SYSCTL_RCGC2_R |= 0x08;           // Port D clock
+  delay = SYSCTL_RCGC2_R;           // wait 3-5 bus cycles
+  GPIO_PORTD_DIR_R |= 0x09;         // PD3 PD0 output
+  GPIO_PORTD_AFSEL_R &= ~0x09;      // not alternative
+  GPIO_PORTD_AMSEL_R &= ~0x09;      // no analog
+  GPIO_PORTD_PCTL_R &= ~0x0000F00F; // bits for PD3, PD0
+  GPIO_PORTD_DEN_R |= 0x09;         // enable PD3, PD0
+	}
+	
+	void PortF_Init(void)
+	{
+		volatile unsigned long delay;
+		SYSCTL_RCGC2_R |= 0x00000020;     // 1) activate clock for Port F
+  delay = SYSCTL_RCGC2_R;           // allow time for clock to start
+  GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock GPIO Port F
+  GPIO_PORTF_CR_R = 0x1F;           // allow changes to PF4-0
+  // only PF0 needs to be unlocked, other bits can't be locked
+  GPIO_PORTF_AMSEL_R = 0x00;        // 3) disable analog on PF
+  GPIO_PORTF_PCTL_R = 0x00000000;   // 4) PCTL GPIO on PF4-0
+  GPIO_PORTF_DIR_R = 0x0E;          // 5) PF4,PF0 in, PF3-1 out
+  GPIO_PORTF_AFSEL_R = 0x00;        // 6) disable alt funct on PF7-0
+  GPIO_PORTF_PUR_R = 0x11;          // enable pull-up on PF0 and PF4
+  GPIO_PORTF_DEN_R = 0x1F;          // 7) enable digital I/O on PF4-0
+	}
+	
+	void Delay(unsigned long time)
+	{
+		unsigned long i;
+		while(time>0)
+		{i=1333333;
+			while(i>0)
+			{i=i-1;
+			}
+			time=time-1;
+		}
+	}
+
+		
+	
+	
+	
